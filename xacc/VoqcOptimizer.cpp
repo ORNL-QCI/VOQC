ff064c2be98f7971d3a3eb1fa05ef7e6e276a5df
Thien Nguyen <nguyentm@ornl.gov>
2020-08-06 18:57:45 +0000

    Completed VOQC plugin
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- xacc/VoqcOptimizer.cpp
+++ xacc/VoqcOptimizer.cpp
@@ -1,16 +1,41 @@
-#include "voqc.h"
 #include "VoqcOptimizer.hpp"
+#include "xacc.hpp"
+#include <cstdio>
+#include <filesystem>
+
+// VOQC entry-point
+extern "C" {
+void voqc(const char* in_file, const char* out_file);
+}
+
 namespace xacc {
 namespace quantum {
 void VoqcCircuitOptimizer::apply(std::shared_ptr<CompositeInstruction> program, const std::shared_ptr<Accelerator> accelerator, const HeterogeneousMap& options) 
 {
-  std::cout << "Hello apply \n";
-  // TODO:
-  // Steps:
   // (1) Use Staq to translate the program to OpenQASM
+  auto staq = xacc::getCompiler("staq");
+  const auto src = staq->translate(program);
+  
   // (2) Write OpenQASM to temp file
+  const std::string in_fName = std::tmpnam(nullptr);
+  const std::string out_fName = std::tmpnam(nullptr);
+  std::ofstream inFile(in_fName);
+  inFile << src;
+  inFile.close();
+
   // (3) Use voqc(in_file, out_file) to optimize the OpenQASM circuit.
+  voqc(in_fName.c_str(), out_fName.c_str());
+  std::ifstream outFile(out_fName);
+  std::string optSrc((std::istreambuf_iterator<char>(outFile)), std::istreambuf_iterator<char>());
+
   // (4) Use Staq to re-compile the output file.
+  auto ir = staq->compile(optSrc);
+  // reset the program and add optimized instructions
+  program->clear();
+  program->addInstructions(ir->getComposites()[0]->getInstructions());
+  
   // (5) Clean-up the temporary files.
+  remove(in_fName.c_str());
+  remove(out_fName.c_str());    
 }
 }}
--- xacc/tests/VOQCTester.cpp
+++ xacc/tests/VOQCTester.cpp
@@ -11,9 +11,59 @@ TEST(VOQCTester, checkSimple)
       T(q[0]);
       T(q[0]);
     })")->getComposite("test_t_t");
-    std::cout << "HOWDY: \n" << program->toString() << "\n";
+    
+    EXPECT_EQ(2, program->nInstructions());
+    EXPECT_EQ("T", program->getInstruction(0)->name());
+    EXPECT_EQ("T", program->getInstruction(1)->name());
+
     irt->apply(program, nullptr);
-    std::cout << "HOWDY: \n" << program->toString() << "\n";
+
+    EXPECT_EQ(1, program->nInstructions());
+    EXPECT_EQ("S", program->getInstruction(0)->name());
+
+
+    program = compiler->compile(R"(__qpu__ void test_t_tdg(qreg q) {
+        T(q[0]);
+        Tdg(q[0]);
+    })")->getComposite("test_t_tdg");
+
+    EXPECT_EQ(2, program->nInstructions());
+    EXPECT_EQ("T", program->getInstruction(0)->name());
+    EXPECT_EQ("Tdg", program->getInstruction(1)->name());
+
+    irt->apply(program, nullptr);
+
+    EXPECT_EQ(0, program->nInstructions());
+
+    program = compiler->compile(R"(__qpu__ void test_conj_merge(qreg q) {
+        H(q[0]);
+        T(q[0]);
+        H(q[0]);
+        X(q[0]);
+        H(q[0]);
+        T(q[0]);
+        H(q[0]);
+    })")->getComposite("test_conj_merge");
+
+    irt->apply(program, nullptr);
+    std::cout << "HOWDY:\n" << program->toString() << "\n"; 
+    EXPECT_EQ(3, program->nInstructions());
+
+    program = compiler->compile(R"(__qpu__ void test_cx_merge(qreg q) {
+      CX(q[0],q[1]);
+      T(q[1]);
+      CX(q[0], q[1]);
+      CX(q[1], q[0]);
+      T(q[0]);
+      CX(q[1],q[0]);
+    })")->getComposite("test_cx_merge");
+    irt->apply(program, nullptr);
+    std::cout << "HOWDY:\n" << program->toString() << "\n"; 
+
+    EXPECT_EQ(3, program->nInstructions());
+    EXPECT_EQ("CNOT", program->getInstruction(0)->name());
+    EXPECT_EQ("S", program->getInstruction(1)->name());
+    EXPECT_EQ("CNOT", program->getInstruction(2)->name());
 }
 
 int main(int argc, char **argv) 
