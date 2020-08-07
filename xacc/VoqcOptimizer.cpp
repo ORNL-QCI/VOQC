5be3bd44292fa89373f6c58ccc1c6de0c5008fc4
Thien Nguyen <nguyentm@ornl.gov>
2020-08-07 00:47:07 +0000

    Handled VOQC gate set
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- xacc/VoqcOptimizer.cpp
+++ xacc/VoqcOptimizer.cpp
@@ -1,5 +1,6 @@
 #include "VoqcOptimizer.hpp"
 #include "xacc.hpp"
+#include "InstructionIterator.hpp"
 #include <cstdio>
 #include <filesystem>
 
@@ -8,10 +9,82 @@ extern "C" {
 void voqc(const char* in_file, const char* out_file);
 }
 
+namespace {
+std::string convertRzGate(const std::string& in_src)
+{
+  auto str = in_src;
+  auto start_pos = str.find("rz(", 0);
+  
+  while (start_pos != std::string::npos) 
+  {
+    const auto start_angle = str.find("(", start_pos);
+    const auto stop_angle =  str.find(")", start_pos);
+    const auto originalLength = stop_angle - start_angle - 1;
+    const double angle = std::stod(str.substr(start_angle + 1, originalLength));  
+    const double normalizedAngle = angle / M_PI;
+    const std::string newThetaStr = std::to_string(normalizedAngle) + "*pi";
+    str.replace(start_angle + 1, originalLength, newThetaStr);
+    start_pos = str.find("rz(", stop_angle);
+  }
+  return str;
+}
+
+std::string convertRzqGate(const std::string& in_src)
+{
+  auto str = in_src;
+  auto start_pos = str.find("rzq(", 0);
+  
+  while (start_pos != std::string::npos) 
+  {
+    const auto start_angle = str.find("(", start_pos);
+    const auto stop_angle =  str.find(")", start_pos);
+    const auto originalLength = stop_angle - start_angle - 1;
+    const auto pairStr = str.substr(start_angle + 1, originalLength);  
+    const auto delim_pos = pairStr.find(",", 0);
+    const auto num = static_cast<double>(std::stoi(pairStr.substr(0, delim_pos)));
+    const auto den = static_cast<double>(std::stoi(pairStr.substr(delim_pos + 1)));
+    const double angle = (num / den) * M_PI;
+    const std::string angleStr = std::to_string(angle);
+    str.replace(start_pos + 2, originalLength + 2, "(" + angleStr);
+    start_pos = str.find("rzq(", stop_angle);
+  }
+  return str;
+}
+
+const std::vector<std::string> SUPPORTED_GATES = {
+  "T", "Tdg",
+  "S", "Sdg",
+  "Z", "Rz",
+  "H", "X", "CNOT"
+};
+
+bool allGatesSupported(std::shared_ptr<xacc::CompositeInstruction> program)
+{
+  xacc::InstructionIterator it(program);
+  while (it.hasNext())
+  {
+    auto nextInst = it.next();
+    if (nextInst->isEnabled() && !nextInst->isComposite())
+    {
+      if (!xacc::container::contains(SUPPORTED_GATES, nextInst->name()))
+      {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+}
+
 namespace xacc {
 namespace quantum {
 void VoqcCircuitOptimizer::apply(std::shared_ptr<CompositeInstruction> program, const std::shared_ptr<Accelerator> accelerator, const HeterogeneousMap& options) 
 {
+  if(!allGatesSupported(program))
+  {
+    return;
+  }
+
   // (1) Use Staq to translate the program to OpenQASM
   auto staq = xacc::getCompiler("staq");
   const auto src = staq->translate(program);
@@ -26,16 +99,16 @@ void VoqcCircuitOptimizer::apply(std::shared_ptr<CompositeInstruction> program, 
   const std::string out_fName(outTemplate);
 
   std::ofstream inFile(in_fName);
-  inFile << src;
+  inFile << convertRzGate(src);
   inFile.close();
 
   // (3) Use voqc(in_file, out_file) to optimize the OpenQASM circuit.
   voqc(in_fName.c_str(), out_fName.c_str());
   std::ifstream outFile(out_fName);
   std::string optSrc((std::istreambuf_iterator<char>(outFile)), std::istreambuf_iterator<char>());
-
+  
   // (4) Use Staq to re-compile the output file.
-  auto ir = staq->compile(optSrc);
+  auto ir = staq->compile(convertRzqGate(optSrc));
   // reset the program and add optimized instructions
   program->clear();
   program->addInstructions(ir->getComposites()[0]->getInstructions());
--- xacc/tests/VOQCTester.cpp
+++ xacc/tests/VOQCTester.cpp
@@ -64,6 +64,39 @@ TEST(VOQCTester, checkSimple)
     EXPECT_EQ("CNOT", program->getInstruction(0)->name());
     EXPECT_EQ("S", program->getInstruction(1)->name());
     EXPECT_EQ("CNOT", program->getInstruction(2)->name());
+
+    program = compiler->compile(R"(__qpu__ void test_rz_merge(qreg q) {
+     Rz(q[0],pi/16);
+     Rz(q[0], pi/16);
+    })")->getComposite("test_rz_merge");
+    irt->apply(program, nullptr);
+    EXPECT_EQ(1, program->nInstructions());
+    EXPECT_EQ("Rz", program->getInstruction(0)->name());
+    EXPECT_NEAR(.3927, program->getInstruction(0)->getParameter(0).as<double>(), 1e-3);
+    
+    auto staq = xacc::getCompiler("staq");
+    program = staq->compile(R"(OPENQASM 2.0;
+                      include "qelib1.inc";
+                      qreg q[4];
+                      creg c[4];
+                      x q[1];
+                      h q[0];
+                      ccx q[0],q[1],q[3];
+                      x q[0];
+                      )")->getComposites()[0];;
+    std::cout << "BEFORE:\n" << program->toString() << "\n"; 
+    irt->apply(program, nullptr);
+    std::cout << "AFTER:\n" << program->toString() << "\n"; 
+
+    // Test unsupported gate:
+    program = compiler->compile(R"(__qpu__ void test_unsupported_gates(qreg q) {
+     Rx(q[0],pi/16);
+     Ry(q[0], pi/16);
+    })")->getComposite("test_unsupported_gates");
+    irt->apply(program, nullptr);
+    EXPECT_EQ(2, program->nInstructions());
+    EXPECT_EQ("Rx", program->getInstruction(0)->name());
+    EXPECT_EQ("Ry", program->getInstruction(1)->name());
 }
 
 int main(int argc, char **argv) 
